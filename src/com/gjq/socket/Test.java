3eb025e9dc7ea359d9951f292e98c81889e0d2a6
1574432191 <54255422+1574432191@users.noreply.github.com>
2019-09-10 13:39:00 +0800

    Update Test.java

--- src/com/gjq/socket/Test.java
+++ src/com/gjq/socket/Test.java
@@ -1,5 +1,9 @@
 package com.gjq.socket;
-
+/*
+*该程序的启动是先启动Test里面的主函数，开启服务端
+*然后再在dos窗口输入telnet localhost 端口号 连接到服务端
+*输入用户名和密码就能够实现通信
+*/
 public class Test {
 	public static void main(String[] args) {
 		MyServer mServer = new MyServer();
